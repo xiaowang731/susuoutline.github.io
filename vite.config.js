776c2669a812ffc80a4d59ae2ffa15daf7ad9f74
susu <meiyou>
2024-12-22 16:23:13 +0800

    加载动画完成

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from 'path';
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/susuoutline.github.io/',
-  // base: "/",
+  // base: '/susuoutline.github.io/',
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src')  // 配置 @ 别名指向 src 目录
-    }
-  }
+      "@": path.resolve(__dirname, "src"), // 配置 @ 别名指向 src 目录
+    },
+  },
 });
